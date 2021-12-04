7fbd7952e32472539698278c2fb0b98e9ac50a5f
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-12-04 09:33:40 +0700

    Update final_b.py

--- KongNew/Practice_Final/final_b.py
+++ KongNew/Practice_Final/final_b.py
@@ -7,9 +7,11 @@ for i in range(0, len(word_ls), 2):
     word1 = word_ls[i]
     word2 = word_ls[i+1]
 
+    print(word1)
+    print(word2)
     for j in range(len(word1)):
         if word1[j] == word2[j]:
             print(".", end="")
         else:
             print("*", end="")
-    print()
+    print("\n")
